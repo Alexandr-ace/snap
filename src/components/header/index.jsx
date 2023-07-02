279e561818e3051f2d3d5bd92f38f9dd282d1310
Alexandr-ace <syakubin@bk.ru>
2023-07-03 00:10:15 +0300

    new file:   src/components/button/index.jsx 	new file:   src/components/header/constants.js 	modified:   src/components/header/index.jsx 	new file:   src/components/menu-item/index.jsx 	modified:   src/components/nav-item/index.jsx 	new file:   src/components/nav-menu/index.jsx 	modified:   tailwind.config.js

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { ReactComponent as LogoIcon } from "../../images/logo.svg";
 import NavItem from "../nav-item";
+import NavMenu from "../nav-menu";
+import { COMPANY, FEATURES } from "./constants";
+import Button from "../button";
 
 const Header = () => {
   return (
     <header className="flex">
       <LogoIcon />
-      <nav>
-        <NavItem />
-        <NavItem />
-        <NavItem />
+      <nav className="ml-8 flex  items-center space-x-6">
+        <NavItem text="Feature">
+          <NavMenu items={FEATURES} />
+        </NavItem>
+        <NavItem text="Company">
+          <NavMenu items={COMPANY} />
+        </NavItem>
+        <NavItem text="Careers" />
+        <NavItem text="About" />
       </nav>
+      <div className="ml-auto flex space-x-5">
+        <Button>Loggin</Button>
+        <Button hasborder={true}>Register</Button>
+      </div>
     </header>
   );
 };
--- src/components/nav-item/index.jsx
+++ src/components/nav-item/index.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as ArrowDownIcon } from "../../images/icon-arrow-down.svg";
+import { ReactComponent as ArrowUpIcon } from "../../images/icon-arrow-up.svg";
 
-const NavItem = ({ text = "" }) => {
+const NavItem = ({ text = "asd", children }) => {
+  const [selected, setSelected] = useState("");
   return (
     <div className="relative">
-      <div className="flex cursor-pointer space-x-2 ">
-        <span className="text-medium">{text}</span>
+      <div className="flex cursor-pointer items-center space-x-2">
+        <span
+          className="text-medium-gray hover:text-almost-black "
+          onClick={() => {
+            children && setSelected(text !== selected ? text : "");
+          }}
+        >
+          {text}
+        </span>
+        {children && selected !== text && <ArrowDownIcon />}
+        {children && selected === text && <ArrowUpIcon />}
       </div>
+      {selected && children}
     </div>
   );
 };
